821fb238d1645bf904ec390839f27e9a05b9d4b1
Mohammad Jafar Uddin <mdjafaruddinsomrat@gmail.com>
2022-04-25 02:45:16 +0200

    Create Onload handler

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random rbg color by clicking a button
+ */
+
+// Steps 
+
+// Step 1 Create Onload handler 
+
+window.onload = () => {
+   main();
+};
+
+function main(){
+
+
+}
+
